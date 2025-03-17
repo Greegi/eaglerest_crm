c52755d0ca92ba124cb5e4b8cccd5055e5d3417f
Grigroriy <greegi2711@gmail.com>
2025-03-17 20:17:00 +0400

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import {componentTagger} from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/greegi.github.io/" : "/",  // Укажи имя репо
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
